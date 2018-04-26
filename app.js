445daa2523d7c8bad74b8d066fc1ba3d76402530
Rulo Kobashikawa <akobashikawa@gmail.com>
2018-04-25 21:18:31 -0500

    Starting
    First commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const feathers = require('@feathersjs/feathers');
+const app = feathers();
+
+// register service
+app.use('todos', {
+  // implements method
+  async get(name) {
+    return {
+      name,
+      text: `You have to do ${name}`
+    }
+  }
+});
+
+// use service
+async function getTodo(name) {
+  const service = app.service('todos');
+  const todo = await service.get(name);
+
+  console.log(todo);
+}
+
+getTodo('dishes');
